bc2543bc3aaf88602ef2e8e4a860d0fde7724331
Evgeny Ukhanov <evgeny@aurora.dev>
2023-05-03 06:33:36 +0200

    Tests: added aurora=workspace for utility

--- eth-connector-tests/src/lib.rs
+++ eth-connector-tests/src/lib.rs
@@ -1,6 +1,6 @@
-#[cfg(test)]
-mod connector;
-#[cfg(all(test, feature = "migration-tests"))]
-mod migration;
+// #[cfg(test)]
+// mod connector;
+// #[cfg(all(test, feature = "migration-tests"))]
+// mod migration;
 #[cfg(test)]
 pub mod utils;
