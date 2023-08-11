7b9fd39047b206ed1103a5b4c832e5c70a755116
karim-en <karim@aurora.dev>
2023-08-11 14:56:24 +0100

    Use doc comments

--- eth-connector/src/fee.rs
+++ eth-connector/src/fee.rs
@@ -14,24 +14,24 @@ pub enum FeeType {
 
 #[derive(BorshDeserialize, BorshSerialize, Debug, Clone, Serialize, Deserialize, Copy)]
 pub struct Fee {
-    // Fee percentage in 6 decimal precision (10% -> 0.1 * 10e6 -> 100_000)
+    /// Fee percentage in 6 decimal precision (10% -> 0.1 * 10e6 -> 100_000)
     pub fee_percentage: U128,
-    // The minimum fee amount for transfer
+    /// The minimum fee amount for transfer
     pub lower_bound: Option<U128>,
-    // The maximum fee amount for transfer
+    /// The maximum fee amount for transfer
     pub upper_bound: Option<U128>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct FeeStorage {
-    // Enables a default fee for deposit transfers
+    /// Enables a default fee for deposit transfers
     pub deposit_fee: Option<Fee>,
-    // Enables a default fee for withdrawal transfers
+    /// Enables a default fee for withdrawal transfers
     pub withdraw_fee: Option<Fee>,
-    // Override the default fee for deposit transfers that target silos
+    /// Override the default fee for deposit transfers that target silos
     pub deposit_fee_per_silo: UnorderedMap<AccountId, Fee>,
-    // Override the default fee for withdrawal transfers initiated by silos
+    /// Override the default fee for withdrawal transfers initiated by silos
     pub withdraw_fee_per_silo: UnorderedMap<AccountId, Fee>,
-    // If set, then fee minted to the `fee_owner` account
+    /// If set, then fee minted to the `fee_owner` account
     pub fee_owner: Option<AccountId>,
 }
